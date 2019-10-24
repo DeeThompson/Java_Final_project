e0e9cbf316e0eeafa982276df042cac3e70ba4a3
deethompson <devon.thompson0@gmail.com>
2019-10-24 15:26:03 -0400

    second commit

---
+++ src/main/java/com/example/demo/Admin.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Admin {
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private long id;
+        private String addalbum;
+        OneToOne(cascade =CascadeType.ALL)
+            @JoinColumn(name ="album_id")
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddalbum() {
+        return addalbum;
+    }
+
+    public void setAddalbum(String addalbum) {
+        this.addalbum = addalbum;
+    }
+}
